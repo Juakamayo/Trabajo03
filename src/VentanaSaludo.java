b5ead4f9795256025df01520964df7799f6b4ff7
Juakamayo <j.pizarro02@ufromail.cl>
2025-09-04 23:17:37 -0400

    1.0

---
+++ src/VentanaSaludo.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class VentanaSaludo {
+    public static void main(String[] args) {
+
+        JFrame ventana = new JFrame ( " App de Saludo ICC490 " ) ;
+        ventana . setSize (600 , 400) ;
+        ventana . setDefaultCloseOperation ( JFrame . EXIT_ON_CLOSE ) ;
+        ventana . setLayout ( null ) ;
+        ventana . getContentPane().setBackground(Color.decode("#FFB6C1"));
+        JTextField campoTexto = new JTextField () ;
+        campoTexto . setBounds (50 , 30 , 200 , 25) ;
+        JButton botonSaludar = new JButton ( " Saludar " ) ;
+        botonSaludar . setBounds (270 , 30 , 150 , 75) ;
+        JLabel etiquetaSaludo = new JLabel ( " Aqui aparecera su saludo " ) ;
+        etiquetaSaludo . setBounds (50 , 80 , 300 , 25) ;
+        botonSaludar . addActionListener ( e -> {
+            String nombre = campoTexto . getText () ;
+            if (nombre.trim().isEmpty()) {
+
+                JOptionPane.showMessageDialog(null, "Por favor ingresa tu nombre.");
+            } else {
+                etiquetaSaludo.setText("¡Hola, " + nombre +"!");
+            }
+        }) ;
+
+
+        campoTexto.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    botonSaludar.doClick(); // Simula un clic en el botón
+                }
+            }
+        });
+
+
+        ventana . add ( campoTexto ) ;
+        ventana . add ( botonSaludar ) ;
+        ventana . add ( etiquetaSaludo ) ;
+        ventana . setLocationRelativeTo ( null ) ;
+        ventana . setVisible ( true ) ;
+
+    }
+}
